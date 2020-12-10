8e5435cbc9062d5f85f58f01c4c43bba560f90e3
Loïc Seguin <loicseguin74@gmail.com>
2020-12-10 14:18:40 +0100

    FormSearch modification

--- src/components/Header/FormSearch.js
+++ src/components/Header/FormSearch.js
@@ -21,31 +21,13 @@ export default function FormSeach({ setPhotos, setPartie }) {
             dataGame(data);
         })
 
-        /*
-
-        const API_KEY = process.env.REACT_APP_API_KEY;
-
-        fetch(`https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${title}&per_page=15`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            if( data.results.length < 15 || number === undefined ) {
-                alert("Le thème n'a pas été trouvé, le nombre n'a pas été défini, veuillez saisir une nouvelle requête !");
-                setTitle("");
-                return;
-            }
-            dataGame(data);
-        }) 
-
-        */
-
     }
 
     const dataGame = (data) => {
         let arr = [];
         let newIndex = 0;
         let dataPhotos = data.results.slice(0, number);
-        dataPhotos.map((photo) => {
+        dataPhotos.forEach((photo) => {
             for(let i = 0; i < 2; i++) {
                 arr.push({url: photo.urls.regular, class: photo.urls.regular.slice(34, 44), id: `photo-`+newIndex, user: photo.user.name, link: photo.user.links.html});
                 newIndex++;
